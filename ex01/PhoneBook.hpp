31b25973fad292193f90ca28802bf5d43c838907
Nikolaos Sarmadakis <sarmadakis.nikos@gmail.com>
2025-01-27 15:56:02 +0100

    Class created

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: nsarmada <nsarmada@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/01/27 12:33:07 by nsarmada      #+#    #+#                 */
-/*   Updated: 2025/01/27 12:40:53 by nsarmada      ########   odam.nl         */
+/*   Updated: 2025/01/27 15:55:10 by nsarmada      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,31 @@
 
 #include <string>
 
+class Contact
+{
+	private:
+		std::string FirstName;
+		std::string LastName;
+		std::string NickName;
+		std::string PhoneNumber;
+		std::string DarkestSecret;
+	
+	Public:
+		Contact(); //Constructor
+	
+	//Setter functions
+	void setFirstName(const std::string& FirstName);
+	void setLastName(const std::string& LastName);
+	void setNickName(const std::string& NickName);
+	void setPhoneNumber(const std::string& PhoneNumber);
+	void setDarkestSecret(const std::string& DarkestSecret);
+
+	//getter functions
+	std::string getFirstName() const;
+	std::string getLastName() const;
+	std::string getNickName() const;
+	std::string getPhoneNumber() const;
+	std::string getDarkestSecret() const;
+}
 
 #endif
